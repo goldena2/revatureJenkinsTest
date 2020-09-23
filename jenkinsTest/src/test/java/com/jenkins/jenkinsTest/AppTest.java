49acd886e4b1d0200ec4179d92c43977a030e77f
goldena2 <goldena2@wwu.edu>
2020-09-23 13:38:40 -0400

    wat

--- jenkinsTest/src/test/java/com/jenkins/jenkinsTest/AppTest.java
+++ jenkinsTest/src/test/java/com/jenkins/jenkinsTest/AppTest.java
@@ -1,40 +1,19 @@
 package com.jenkins.jenkinsTest;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+import static org.junit.jupiter.api.Assertions.*;
+
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        
+    @Test
+    public void test1() {
+    	assertTrue(false);
     }
     
+    @Test
+    public void test2(){
+    	assertFalse(true);
+    }
     
 }
